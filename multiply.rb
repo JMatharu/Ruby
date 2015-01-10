86aeffbfc898f0cb6d9875fc719275d64af3cf32
JMatharu <jagdeepmatharu88@gmail.com>
2015-01-09 20:46:30 -0500

    multiply

---
+++ multiply.rb
@@ -0,0 +1,12 @@
+#Ask the user to enter two numbers as input and print their product.
+
+print "Enter First number : "
+  a = gets.to_f
+print "Enter Second number : "
+  b = gets.to_f
+
+result = a * b
+
+print "Multiplication result is : #{result}."
+
+
