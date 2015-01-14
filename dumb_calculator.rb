e679b1ab60f2c9403c4090e5fc5204483fe9a0f7
JMatharu <jagdeepmatharu88@gmail.com>
2015-01-13 20:07:49 -0500

    Calc_update

--- dumb_calculator.rb
+++ dumb_calculator.rb
@@ -1,13 +1,13 @@
 # Make a simple numeric calculator. It should prompt the user for three numbers. Then add the numbers together and divide by 2. Display the result.
 # Your program must support numbers with decimals and not just integers.
 
-print "Enter First Number : "
+print "Enter First Number: "
   a = gets.to_f
 
-print "Enter Second Number : "
+print "Enter Second Number: "
   b = gets.to_f
 
-print "Enter Third Number : "
+print "Enter Third Number: "
   c = gets.to_f
 
 result = (a+b+c)/2
